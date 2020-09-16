877f6d9ac24a11753502878a67944853c11e65c3
Joshua Tucker <the1jtux@gmail.com>
2020-09-15 22:57:28 -0400

    Step 14 - NoteEntity added with Migration

---
+++ ElevenNote.Data/Entities/NoteEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ElevenNote.Data.Entities
+{
+    public class NoteEntity
+    {
+        [Key]
+        public int NoteId { get; set; }
+
+        [Required]
+        public int OwnerId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
---
+++ ElevenNote.Data/Migrations/20200916025554_AddedNoteEntity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ElevenNote.Data.Migrations
+{
+    public partial class AddedNoteEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notes",
+                columns: table => new
+                {
+                    NoteId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OwnerId = table.Column<int>(nullable: false),
+                    Title = table.Column<string>(nullable: false),
+                    Content = table.Column<string>(nullable: false),
+                    CreatedUtc = table.Column<DateTimeOffset>(nullable: false),
+                    ModifiedUtc = table.Column<DateTimeOffset>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notes", x => x.NoteId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notes");
+        }
+    }
+}
